451b4948b6add3d054b9ce7cd4f5935d96ab88cf
Vincent50703 <stes110036@stes.tp.edu.tw>
2025-11-17 20:47:29 +0800

    Update ex4-1.py

--- ex4-1.py
+++ ex4-1.py
@@ -10,3 +10,16 @@ print(f"除法：{a} / {b} = {a / b}")
 print(f"整數除法：{a} // {b} = {a // b}")
 print(f"餘數：{a} % {b} = {a % b}")
 print(f"次方：{a} ** {b} = {a ** b}")
+
+age = int(input("please input a num "))
+
+if age >= 18:
+    print("您已成年，可以投票！")
+else:
+    print("您未成年，無法投票。")
+
+if len(username) > 0:
+  print(f"Welcome, {username}!")
+else:
+  print("Error: Username cannot be empty")
+
