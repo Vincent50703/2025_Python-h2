0401eed056176db75c080113ff6ac3387bb5c569
Vincent50703 <stes110036@stes.tp.edu.tw>
2025-11-24 19:19:50 +0800

    Update ex4-1.py

--- ex4-1.py
+++ ex4-1.py
@@ -22,4 +22,15 @@ if len(username) > 0:
   print(f"Welcome, {username}!")
 else:
   print("Error: Username cannot be empty")
+score = 75
 
+if score >= 90:
+  print("Grade: A")
+elif score >= 80:
+  print("Grade: B")
+elif score >= 70:
+  print("Grade: C")
+elif score >= 60:
+  print("Grade: D")
+else:
+    grade = "F"
